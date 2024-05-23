e0a7324749adee1dbffee4e4e9dceffb763e6787
Smith <46734345+hoangthanhnguyen@users.noreply.github.com>
2024-05-23 15:08:17 +0700

    Create https.server.py
    Create https.server.py

---
+++ https.server.py
@@ -0,0 +1,14 @@
+
+# generate server.pem with the following command:
+#    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+# how to run:
+#    python simple-https-server.py
+# in browser:
+#    https://localhost
+
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.serve_forever()
